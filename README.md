1e41f183787c8c29f0210424e170828a6de98724
sumranrai <sumranrai@hotmail.com>
2016-09-09 18:54:01 +0500

    Unit Testing 1

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 # Array.php has introduction to three types of arrays : numeric, associative and multidimensional. for all three cases the dynamic and static arrays are introduced seperately.
 
 # functions.php && UnitTest.php:The functions implemented in functions.php have there unit tests in UnitTest.php.
-#I have implemented a addFunction:
+I have implemented a addFunction:
     Input : two numbers
     Output: sum of the two numbers
     
--- UnitTest.php
+++ UnitTest.php
@@ -2,9 +2,23 @@
     error_reporting(E_ALL);
     ini_set('display_errors', 1);
     require("functions.php");
+function addFunctionTest()
+{
+    echo "muhammad Sumran";
     $ans = addFunction(10, 20);
-if ($ans ==  30) {
+    if ($ans ==  30) {
         echo "test1 passed";
-} else {
+    } else {
         echo "test1 failed";
+        # need to do error handling may be throw an exception
+    }
+    $ans = addFunction(-10, -20);
+    if ($ans ==  -30) {
+        echo "test2 passed";
+    } else {
+        echo "test2 failed";
+        # need to do error handling , may be throw an exception
+    }
+        echo "addFunction works";
 }
+addFunctionTest();
