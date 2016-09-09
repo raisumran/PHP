c757bc55dad7297cdf9a311f0db551c9d1ea62c8
sumranrai <sumranrai@hotmail.com>
2016-09-09 15:20:38 +0500

    adding test

--- test.php
+++ test.php
@@ -1,7 +1,15 @@
 <?php
+    error_reporting(E_ALL);
+    ini_set('display_errors', 1);
+    #Camel Case for variables
     # Integers and Doubles
-    echo "Hello World";
-    print "<br>";
-    $int_var = 12345;
-    $another_int = 1235 + 123.46;
-    echo "$another_int";
+    echo "Hello World <br>";
+    $number = 12345;
+    $anotherNumber = 1235 + 123.46;
+    echo "$anotherNumber"."<br>";
+    #Understanding the concept of literal and variable string
+    $varyStr = "name";
+    $literalStr = 'my $varyStr will print';
+    echo "$literalStr". "<br>";
+    $literalStr = "my $varyStr will print";
+    echo "$literalStr". "<br>";
